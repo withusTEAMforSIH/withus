ce4026a9ddc27470a69880db1f3b667dcd914cd7
Aarya Gupta <aaryyaguptta@gmail.com>
2026-08-23 15:07:25 +0530

    Add DB_PORT to SQLALCHEMY_DATABASE_URI

--- app.py
+++ app.py
@@ -15,8 +15,9 @@ DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_NAME = os.getenv('DB_NAME')
+DB_PORT = os.environ.get('DB_PORT')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
