4b73d6c973727c4f30937ab6aa40f61eafa3c211
Aarya Gupta <aaryyaguptta@gmail.com>
2026-08-23 15:20:47 +0530

    Update socketio.run parameters for deployment

--- app.py
+++ app.py
@@ -999,4 +999,4 @@ def handle_call_end(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+   socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), allow_unsafe_werkzeug=True)
